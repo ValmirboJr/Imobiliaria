60134397393d4d2ed322c01c2fbee8dc311b72a4
ValmirboJr <valmirborgesojr@gmail.com>
2023-11-12 02:00:53 -0300

    Parte de Confirmação de CPF adicionada

--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
-const header = document.querySelector("header");
-
-window.addEventListener("scroll",function(){
-    header.classList.toggle("sticky",window.scrollY>80)
-});
-
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () =>{
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open')
-};
-
-window.onscroll = () =>{
-    menu.classList.remove('bx-x');
-    navbar.classList.remove('open')
-};
+document.addEventListener("DOMContentLoaded", function () {
+    const carrinho = document.querySelector(".h-btn2");
+    const carrinhoLateral = document.createElement("div");
+    carrinhoLateral.className = "carrinho-lateral";
+  
+    function adicionarAoCarrinho(nome) {
+      const item = document.createElement("div");
+      item.className = "item-carrinho";
+      item.innerText = nome;
+      carrinhoLateral.appendChild(item);
+    }
+  
+    const botoesAdicionar = document.querySelectorAll(".adicionar-ao-carrinho");
+    botoesAdicionar.forEach(function (botao) {
+      botao.addEventListener("click", function (e) {
+        e.preventDefault();
+        const nomeResidencia = this.parentNode.parentNode.querySelector("h5").innerText;
+        adicionarAoCarrinho(nomeResidencia);
+      });
+    });
+  
+    carrinho.addEventListener("click", function () {
+      if (carrinhoLateral.childElementCount > 0) {
+        if (!document.body.contains(carrinhoLateral)) {
+          document.body.appendChild(carrinhoLateral);
+        }
+      }
+    });
+  });
+''  
